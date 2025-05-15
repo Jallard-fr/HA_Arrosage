384deb167160b798c0737a6097b7b2a11cece177
Jallard-fr <jbaptiste.allard@gmail.com>
2025-05-15 13:17:32 +0200

    Create const.py

---
+++ custom_components/arrosage/const.py
@@ -0,0 +1,8 @@
+DOMAIN = "arrosage"
+CONF_SENSOR = "threshold_sensor"
+CONF_THRESHOLD = "threshold"
+CONF_DURATION = "duration"
+CONF_PRECIP_SENSOR = "precipitation_sensor"
+CONF_PRECIP_THRESHOLD = "precipitation_threshold"
+CONF_CHECK_INTERVAL = "check_interval"
+DEFAULT_CHECK_INTERVAL = 30  # en secondes
